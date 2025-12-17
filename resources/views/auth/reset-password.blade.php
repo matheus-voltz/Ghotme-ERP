ec35695856da96b202a1712e7c6e2954c0593096
Matheus <grafit_8@hotmail.com>
2025-12-17 07:42:17 -0300

    resetar senha

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -40,9 +40,8 @@ $customizerHidden = 'customizer-hide';
     <!-- Reset Password -->
     <div class="d-flex col-12 col-xl-4 align-items-center authentication-bg p-6 p-sm-12">
       <div class="w-px-400 mx-auto mt-12 pt-5">
-        <h4 class="mb-1">Reset Password 🔒</h4>
-        <p class="mb-6"><span class="fw-medium">Your new password must be different from previously used
-            passwords</span></p>
+        <h4 class="mb-1">Resetar Senha 🔒</h4>
+        <p class="mb-6"><span class="fw-medium">Sua nova senha deve ser diferente das senhas anteriormente usadas</span></p>
         <form id="formAuthentication" class="mb-6" action="{{ route('password.update') }}" method="POST">
           @csrf
           <input type="hidden" name="token" value="{{ $request->route('token') }}">
@@ -57,7 +56,7 @@ $customizerHidden = 'customizer-hide';
             @enderror
           </div>
           <div class="mb-6 form-password-toggle">
-            <label class="form-label" for="password">New Password</label>
+            <label class="form-label" for="password">Nova Senha</label>
             <div class="input-group input-group-merge @error('password') is-invalid @enderror">
               <input type="password" id="password" class="form-control @error('password') is-invalid @enderror"
                 name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
@@ -71,7 +70,7 @@ $customizerHidden = 'customizer-hide';
             @enderror
           </div>
           <div class="mb-6 form-password-toggle">
-            <label class="form-label" for="confirm-password">Confirm Password</label>
+            <label class="form-label" for="confirm-password">Confirmar Senha</label>
             <div class="input-group input-group-merge">
               <input type="password" id="confirm-password" class="form-control" name="password_confirmation"
                 placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
@@ -79,12 +78,12 @@ $customizerHidden = 'customizer-hide';
               <span class="input-group-text cursor-pointer"><i class="icon-base ti tabler-eye-off"></i></span>
             </div>
           </div>
-          <button type="submit" class="btn btn-primary d-grid w-100 mb-6">Set new password</button>
+          <button type="submit" class="btn btn-primary d-grid w-100 mb-6">Definir nova senha</button>
           <div class="text-center">
             @if (Route::has('login'))
             <a href="{{ route('login') }}" class="d-flex justify-content-center">
               <i class="icon-base ti tabler-chevron-left scaleX-n1-rtl me-1_5"></i>
-              Back to login
+              Voltar para login
             </a>
             @endif
           </div>
