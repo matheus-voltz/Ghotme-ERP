d6c138d8c2945a03e579fc8ef31763e6530c763a
Matheus <grafit_8@hotmail.com>
2026-01-21 23:21:32 -0300

    Refactor code structure for improved readability and maintainability

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -13,20 +13,6 @@
       0 => 'Laravel\\Jetstream\\JetstreamServiceProvider',
     ),
   ),
-  'laravel/pail' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Pail\\PailServiceProvider',
-    ),
-  ),
-  'laravel/sail' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Sail\\SailServiceProvider',
-    ),
-  ),
   'laravel/sanctum' => 
   array (
     'providers' => 
@@ -59,29 +45,11 @@
       0 => 'Carbon\\Laravel\\ServiceProvider',
     ),
   ),
-  'nunomaduro/collision' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
-    ),
-  ),
   'nunomaduro/termwind' => 
   array (
     'providers' => 
     array (
       0 => 'Termwind\\Laravel\\TermwindServiceProvider',
-    ),
-  ),
-  'pixinvent/vuexy-laravel-bootstrap-jetstream' => 
-  array (
-    'aliases' => 
-    array (
-      'Jetstrap' => 'Pixinvent\\Jetstrap\\JetstrapFacade',
-    ),
-    'providers' => 
-    array (
-      0 => 'Pixinvent\\Jetstrap\\JetstrapServiceProvider',
     ),
   ),
 );
